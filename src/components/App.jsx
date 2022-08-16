69b860ca155354786f43a457b46acffae9dd4b1c
Halyna Melnykova <galyamelnykova@gmail.com>
2022-08-16 23:05:24 +0300

    task 1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  formSubmitHeandler = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => {
+      return { contacts: [newContact, ...contacts] };
+    });
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <>
+        <ContactForm onSubmit={this.formSubmitHeandler} />
+        <h2>Contacts</h2>
+        <ContactList contacts={contacts} />
+      </>
+    );
+  }
+}
+
+export default App;
