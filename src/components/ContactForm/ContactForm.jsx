de8d1a35903fbf17c58f00841a050f0da38a85e2
Halyna Melnykova <galyamelnykova@gmail.com>
2022-08-17 19:37:51 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import s from './ContactForm.module.css';
 
 class ContactForm extends Component {
@@ -63,3 +64,7 @@ class ContactForm extends Component {
 }
 
 export default ContactForm;
+
+ContactForm.propTypes = {
+  formSubmitHeandler: PropTypes.func,
+};
